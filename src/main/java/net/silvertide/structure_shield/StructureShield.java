fa96c9fbd82cad0a61a172645b18f0a99adaaa9d
Silvertide7 <natephillips801@gmail.com>
2025-12-10 00:57:27 -0700

    Cleanup

--- src/main/java/net/silvertide/structure_shield/StructureShield.java
+++ src/main/java/net/silvertide/structure_shield/StructureShield.java
@@ -7,7 +7,6 @@ import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.neoforge.common.NeoForge;
 
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
@@ -19,7 +18,6 @@ public class StructureShield {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public StructureShield(IEventBus modEventBus, ModContainer modContainer) {
-        NeoForge.EVENT_BUS.register(this);
     }
 
 }
