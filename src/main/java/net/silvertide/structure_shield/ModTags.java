b1fc4cec488cbf6dce3e89818c83f3d98188cf2b
Silvertide7 <natephillips801@gmail.com>
2025-12-11 18:37:27 -0700

    Initial event implementation

---
+++ src/main/java/net/silvertide/structure_shield/ModTags.java
@@ -0,0 +1,15 @@
+package net.silvertide.structure_shield;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.levelgen.structure.Structure;
+
+public class ModTags {
+    public static final TagKey<Block> STRUCTURE_SHIELD_BYPASS =
+            TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(StructureShield.MODID, "structure_shield_bypass"));
+
+    public static final TagKey<Structure> STRUCTURE_SHIELD_PROTECTION =
+            TagKey.create(Registries.STRUCTURE, ResourceLocation.fromNamespaceAndPath(StructureShield.MODID, "structure_shield_protection"));
+}
